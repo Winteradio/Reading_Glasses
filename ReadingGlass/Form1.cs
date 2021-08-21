ae46e0120eb2a9e5c922f2baf400b561d8890d76
Winteradio <tot4766@hanyang.ac.kr>
2021-08-21 12:47:25 +0900

    [마우스 휠에 따른 이미지 확대 및 축소]

---
+++ ReadingGlass/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadingGlass
+{
+    public partial class Form1 : Form
+    {
+        Image img;
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.pictureBox1.MouseWheel += pictureBox1_MouseWheel;
+        }
+        private void pictureBox1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (e.Delta > 0)
+            {
+                pictureBox1.Width += 50;
+                pictureBox1.Height += 50;
+            }
+            else
+            {
+                pictureBox1.Width -= 50;
+                pictureBox1.Height -= 50;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs g)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+                img = pictureBox1.Image;
+            }
+        }
+    }
+}
